1e919fee954e96a9796a864f3c20ca5919bc3571
ShMcK <Shawn.McKay@hotmail.com>
2015-02-09 12:29:45 +0900

    demo setup

--- demo/src/app/index.js
+++ demo/src/app/index.js
@@ -1,14 +1,25 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('demoApp', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+  angular.module('demo', [
+    /* angular-formly-templates-material dependencies */
+    'formly',
+    'ngMaterial',
+    /* additional */
+    'ngAnimate',
+    'ngTouch',
+    'ngSanitize',
+    'ui.router',
+    'ngPrettyJson',
+    'btford.markdown',
+    /* modules */
+    'md.formFields'
+  ])
+    .config(routerSetup);
 
+  function routerSetup($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-  })
-;
+  }
+
+}());
+
