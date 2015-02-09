10e50e96df24ae346db8bd8cececc83a23525802
ShMcK <Shawn.McKay@hotmail.com>
2015-02-09 11:54:46 +0900

    first build

--- src/modules/angular-formly-templates-material.js
+++ src/modules/angular-formly-templates-material.js
@@ -1,50 +1,65 @@
-angular.module('formlyMaterial', ['formly'], function configFormlyLumx(formlyConfigProvider) {
+(function () {
 	'use strict';
-	var PREFIX = 'md';
-	var FIELDS = [];
+	var USING_TEMPLATES = true;
+	var MODULE_NAME = "formlyMaterial";
+	var PREFIX = "md";
+	var FIELDS = [{
+		"name": "checkbox",
+		"template": "<md-checkbox ng-model=\"model[options.key]\" aria-label=\"{{::options.label}}\">{{::options.label}}</md-checkbox>"
+	}, {
+		"name": "input",
+		"template": "<md-input-container><label>{{::options.templateOptions.label}}</label><input ng-model=\"{{model[options.key]}}\" type=\"{{::options.templateOptions.type}}\"></md-input-container>"
+	}, {
+		"name": "radio",
+		"template": "<md-radio-group ng-model=\"model[options.key]\"><md-radio-button ng-repeat=\"(key, option) in options.templateOptions.options track by $index\" ng-value=\"{{::option.value}}\" aria-label=\"{{ ::option.label }}\">{{::option.label}}</md-radio-button></md-radio-group>"
+	}, {
+		"name": "slider",
+		"template": "<div><md-slider ng-model=\"model[options.key]\" min=\"{{::options.templateOptions.min}}\" max=\"{{::options.templateOptions.max}}\" step=\"{{::options.templateOptions.step}}\" aria-label=\"{{::options.templateOptions.label}}\" id=\"{{(::options.templateOptions.color || \'blue\') + \'-slider\'\"></md-slider></div>"
+	}, {
+		"name": "switch",
+		"template": "<md-switch ng-model=\"model[options.key]\" aria-label=\"{{::options.label}}\" ng-class=\"::options.templateOptions.className || \'\'\">{{::options.label}}</md-switch>"
+	}, {
+		"name": "textarea",
+		"template": "<md-input-container><label>{{::options.label}}</label><textarea ng-model=\"model[options.key]\"></textarea></md-input-container>"
+	}];
 
-	var WRAPPERS = [];
-
-	/**
-	 * Add Wrapper templateUrls
-	 */
-	angular.forEach(WRAPPERS, function (wrapper) {
-		wrapper.templateUrl = getWrapperTemplateUrl(wrapper.name);
-	});
-
-	/**
-	 * Cache Templates
-	 */
-	angular.forEach(FIELDS, function (field) {
-		$templateCache.put(getFieldTemplateName(field.name), field.template);
-	});
-
-	/**
-	 * Prepare Wrappers
-	 */
-	formlyConfigProvider.setWrapper(WRAPPERS);
-	var commonWrappers = [];
-	angular.forEach(WRAPPERS, function (wrapper) {
-		commonWrappers.push(PREFIX + '-' + wrapper.name);
-	});
-
-	/**
-	 * Set Field Types with Wrappers
-	 */
-	angular.forEach(FIELDS, function (field) {
-		formlyConfigProvider.setType({
-			name: PREFIX + '-' + field.name,
-			templateUrl: getFieldTemplateName(field.name),
-			wrapper: commonWrappers
-		});
-	});
-
-
-	function getFieldTemplateName(name) {
-		return 'fields/formly-field-' + PREFIX + '-' + name + '.html';
+	function _wrapperTemplateUrl(name) {
+		return 'wrappers/formly-wrappers-' + PREFIX + '-' + name + '.html';
 	}
 
-	function getWrapperTemplateUrl(name) {
-		return 'wrappers/' + PREFIX + '-wrapper-' + name + '.html';
+	function _fieldTemplateUrl(name) {
+		return 'fields/formly-fields-' + PREFIX + '-' + name + '.html';
 	}
-});
+
+	angular.module(MODULE_NAME, ['formly']).config(setCustomTemplates).run(cacheLumXTemplates);
+	/*@ngInject*/
+	function cacheLumXTemplates($templateCache) {
+		if (usingCustomTemplates) {
+			angular.forEach(FIELDS, function (field) {
+				$templateCache.put('fields/formly-templates-' + PREFIX + '-' + field.name + '.html', field.template);
+			});
+			angular.forEach(WRAPPERS, function (wrapper) {
+				$templateCache.put('wrappers/formly-wrappers-' + PREFIX + '-' + wrapper.name + '.html', wrapper.template);
+			});
+		}
+	}
+
+	/*@ngInject*/
+	function setCustomTemplates(formlyConfigProvider) {
+		if (USING_TEMPLATES) {
+			var wrapperList = [];
+			angular.forEach(WRAPPERS, function (wrapper) {
+				wrapperList.push(PREFIX + '-' + wrapper + '-' + wrapper.name);
+			});
+			angular.forEach(WRAPPERS, function (wrapper) {
+				formlyConfigProvider.setWrapper({
+					name: PREFIX + '-' + wrapper + '-' + wrapper.name,
+					templateUrl: _wrapperTemplateUrl(wrapper.name)
+				});
+			});
+			angular.forEach(FIELDS, function (field) {
+				formlyConfigProvider.setType({name: PREFIX + '-' + field.name, templateUrl: _fieldTemplateUrl(field.name)});
+			});
+		}
+	}
+}());
